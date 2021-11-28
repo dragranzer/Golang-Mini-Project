26e045788055f1585dffa2c082b7ac01f9f1bfad
Ivan Muhammad Nizar <ivannizar@gmail.com>
2021-11-28 19:18:42 +0700

    add bussiness, entity

---
+++ features/peminjamans/entity.go
@@ -0,0 +1,20 @@
+package peminjamans
+
+type Core struct {
+	ID         int    `json:"id" form:"id"`
+	Hari       int    `json:"hari" form:"hari"`
+	TotalHarga string `json:"total_harga" form:"total_harga"`
+	BookID     string `json:"book_id" form:"book_id"`
+	UserID     string `json:"user_id" form:"user_id"`
+}
+
+type Bussiness interface {
+	CreateData(data Core) (resp Core, err error)
+	GetAllData() (resp []Core)
+	// continue the function abtraction
+}
+
+type Data interface {
+	InsertData(data Core) (resp Core, err error)
+	SelectAllData() (resp []Core)
+}
