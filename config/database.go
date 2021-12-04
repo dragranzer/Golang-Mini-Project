ccf909923959da6a4d1f2926ac1e519274b541d6
Ivan Muhammad Nizar <ivannizar@gmail.com>
2021-12-04 18:36:56 +0700

    test github action

--- config/database.go
+++ config/database.go
@@ -10,7 +10,7 @@ var DB *gorm.DB
 func InitDB() {
 
 	var err error
-	db, err := gorm.Open(mysql.Open("root:@tcp(host.docker.internal:3306)/minpro_be?parseTime=true"), &gorm.Config{})
+	db, err := gorm.Open(mysql.Open("sql6456325:alIJAGMBRw@tcp(sql6.freemysqlhosting.net:3306)/sql6456325?parseTime=true"), &gorm.Config{})
 	if err != nil {
 		panic(err)
 	}
